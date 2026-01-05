03ca0bad5dfc27800c649b569401b1e1887d8b7f
aethrasoft <mahfuzislam1695@gmail.com>
2026-01-06 01:03:19 +0600

    ok

--- config/envConfig.ts
+++ config/envConfig.ts
@@ -1,6 +1,7 @@
 export const getBaseUrl = (): string => {
   return (
     process.env.NEXT_PUBLIC_API_BASE_URL ||
+    // "http://localhost:5002/tohfa/api/v1"
     "https://tohfa-pos-backend.vercel.app/tohfa/api/v1"
     // "https://bid-bolt-backend.vercel.app/bidbolt/api/v1"
   );
